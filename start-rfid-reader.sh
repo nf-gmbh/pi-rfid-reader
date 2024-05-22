f7fe9ee8d2b5f2cb359acf9a52382aa00d88b49b
Markus Schubert <markus@toto.io>
2024-05-22 17:42:48 +0200

    update start script

--- start-rfid-reader.sh
+++ start-rfid-reader.sh
@@ -8,17 +8,17 @@ cd "$APP_DIR" || exit
 
 # Check if .venv directory exists, if not create it
 if [ ! -d ".venv" ]; then
-    python -m venv .venv
+    python3 -m venv .venv
 fi
 
 # Activate the virtual environment
 source .venv/bin/activate
 
 # Install requirements
-pip install -r requirements.txt
+pip3 install -r requirements.txt
 
 # Navigate to the source folder (assuming source folder is within APP_DIR)
 cd src || exit
 
 # Run the application
-python app_reader.py --log /var/log/pi-rfid-reader/app.log --port 8080 --timeout 5
+python3 app_reader.py --log /var/log/pi-rfid-reader/app.log --port 8080 --timeout 5
